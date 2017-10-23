3f660bcefc960609016e869bfb48ce70acd4490e
Christina Cornell <cdhorn515@gmail.com>
2017-10-23 17:22:59 -0400

    update readme, start work on reading words from file into list

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mystery Word
+
+Interactive hangman style game played on the command line prompt. Demonstrates ability to read from a file, choose a random value, and keep track of state.
--- hangman.py
+++ hangman.py
@@ -1,6 +1,13 @@
 
+def main():
+    path = "/usr/share/dict/words"
+    word_length_3_to_8 = []
+    with open(path) as file:
+        words = file.split()
+    for word in words:
+        if len(word) >= 3 and len(word) <= 8:
+        word_len_3_to_8.append(word)    
 
 
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
